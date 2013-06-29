aea9cddc71d5890da93d257b1f8d45102f422ad6
Marcio Aguiar <mophoal@gmail.com>
2013-06-28 22:29:33 -0300

    no more globals

--- js/noty/jquery.noty.js
+++ js/noty/jquery.noty.js
@@ -10,17 +10,17 @@
  **/
 /* globals define */
 
-if (typeof Object.create !== 'function') {
-    Object.create = function (o) {
-        function F() {
-        }
-
-        F.prototype = o;
-        return new F();
-    };
-}
-
 define(['jquery'], function($) {
+    
+    if (typeof Object.create !== 'function') {
+        Object.create = function (o) {
+            function F() {
+            }
+    
+            F.prototype = o;
+            return new F();
+        };
+    }
     
     var NotyObject = {
 
